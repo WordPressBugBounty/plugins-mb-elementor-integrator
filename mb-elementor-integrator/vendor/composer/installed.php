4429251030f7e5b208dc227e5f6056c786572157
Sean Murphy <sean@iamseanmurphy.com>
2026-09-17 13:27:41 +0000

    2.2.6

--- mb-elementor-integrator/vendor/composer/installed.php
+++ mb-elementor-integrator/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'wpmetabox/mb-elementor-integrator',
-        'pretty_version' => '2.2.5',
-        'version' => '2.2.5.0',
-        'reference' => 'ac0995d24c40a3cf2f9e44a8e8c0df7444ceb8eb',
+        'pretty_version' => '2.2.6',
+        'version' => '2.2.6.0',
+        'reference' => 'a10a05120d2ab4baa31cff3223de6898c6b102db',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'wpmetabox/mb-elementor-integrator' => array(
-            'pretty_version' => '2.2.5',
-            'version' => '2.2.5.0',
-            'reference' => 'ac0995d24c40a3cf2f9e44a8e8c0df7444ceb8eb',
+            'pretty_version' => '2.2.6',
+            'version' => '2.2.6.0',
+            'reference' => 'a10a05120d2ab4baa31cff3223de6898c6b102db',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
